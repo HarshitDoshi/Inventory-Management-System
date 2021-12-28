4e6bc6607d5cb8437ad09c21398f7474d0f36d63
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2021-12-28 19:05:01 +0530

    [Chore][Project][CustomersApp]: Configure DRF and setup customersapp serializers

---
+++ customersapp/serializers.py
@@ -0,0 +1,13 @@
+from .models import Customer, CustomerGroup
+from rest_framework import serializers
+
+
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'first_name', 'last_name', 'address', 'phone', 'email', 'group_id']
+
+class CustomerGroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CustomerGroup
+        fields = ['id', 'name']
