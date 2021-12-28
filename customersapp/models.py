64f50d5ed78dc5dd5a9edabb65f3200dc9699d5d
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2021-12-28 17:31:02 +0530

    [Chore][CustomersApp]: Define Customer and CustomerGroup models

--- customersapp/models.py
+++ customersapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class CustomerGroup(models.Model):
+    name = models.CharField(
+        verbose_name="Customer Group Name",
+        name="Customer Group Name",
+        db_column="name"
+    )
+
+
+class Customer(models.Model):
+    first_name = models.CharField(
+        verbose_name="First Name",
+        name="First Name",
+        db_column="first_name",
+    )
+    last_name = models.CharField(
+        verbose_name="Last Name",
+        name="Last Name",
+        db_column="last_name",
+    )
+    address = models.TextField()
+    phone = PhoneNumberField()
+    email = models.EmailField()
+    group_id = models.ForeignKey(to=CustomerGroup)
