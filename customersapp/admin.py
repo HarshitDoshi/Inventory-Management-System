cc26f3804eb72050f6541d7bc2b7d6e3cb655531
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2021-12-30 10:05:33 +0530

    [CustomersApp]: Fix models

--- customersapp/admin.py
+++ customersapp/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Customer, CustomerGroup
 
 
-admin.site.register(Customer, CustomerGroup)
+# admin.site.register(Customer, CustomerGroup)
--- customersapp/models.py
+++ customersapp/models.py
@@ -8,11 +8,15 @@ class CustomerGroup(models.Model):
         name="Customer Group Name",
         db_column="name",
         max_length=512,
+        blank=False,
+        null=False,
     )
 
     class Meta:
         managed = True
         db_table = "customer_group"
+        verbose_name = "Customer Group"
+        verbose_name_plural = "Customer Groups"
 
 
 class Customer(models.Model):
@@ -21,18 +25,38 @@ class Customer(models.Model):
         name="First Name",
         db_column="first_name",
         max_length=512,
+        blank=False,
+        null=False,
     )
     last_name = models.CharField(
         verbose_name="Last Name",
         name="Last Name",
         db_column="last_name",
         max_length=512,
+        blank=False,
+        null=False,
     )
-    address = models.TextField()
-    phone = PhoneNumberField()
-    email = models.EmailField()
-    group_id = models.ForeignKey(to=CustomerGroup, on_delete=models.CASCADE)
+    address = models.TextField(
+        blank=True,
+        null=False,
+    )
+    phone = PhoneNumberField(
+        blank=False,
+        null=False,
+    )
+    email = models.EmailField(
+        blank=False,
+        null=False,
+    )
+    group_id = models.ForeignKey(
+        to=CustomerGroup,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
 
     class Meta:
         managed = True
         db_table = "customer"
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
