2326881d3317c2f2b6317a88a7f9ba560bf926c0
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2021-12-31 20:29:08 +0530

    [CustomersApp]: Configure URLs

--- customersapp/urls.py
+++ customersapp/urls.py
@@ -1,10 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CustomersList, CustomerDetails, CustomerGroupsList, CustomerGroupDetails
 
-
-router = routers.DefaultRouter()
 
 urlpatterns = [
-
+    path("customers/", CustomersList.as_view()),
+    path("customers/<int:pk>", CustomerDetails.as_view()),
+    path("customers/groups/", CustomerGroupsList.as_view()),
+    path("customers/groups/<int:pk>", CustomerGroupDetails.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns=urlpatterns)
