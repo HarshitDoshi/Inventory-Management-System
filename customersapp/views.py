0c1644410f5dc16fa31a220e02c58c69742a4b13
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2021-12-31 20:21:27 +0530

    [CustomersApp]: Create views for Customer and CustomerGroup models using Django generics

--- customersapp/views.py
+++ customersapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Customer, CustomerGroup
+from .serializers import CustomerSerializer, CustomerGroupSerializer
+from rest_framework import generics
 
 
+class CustomersList(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerGroupsList(generics.ListCreateAPIView):
+    queryset = CustomerGroup.objects.all()
+    serializer_class = CustomerGroupSerializer
+
+
+class CustomerGroupDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomerGroup.objects.all()
+    serializer_class = CustomerGroupSerializer
