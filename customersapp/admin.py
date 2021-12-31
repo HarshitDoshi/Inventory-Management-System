3534d6d9bb1940b4657a2f3cc89f202fe80fe4fe
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2021-12-31 20:42:30 +0530

    [CustomersApp]: Fix admin dashboard config

--- customersapp/admin.py
+++ customersapp/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Customer, CustomerGroup
 
 
-# admin.site.register(Customer, CustomerGroup)
+admin.site.register(Customer)
+admin.site.register(CustomerGroup)
