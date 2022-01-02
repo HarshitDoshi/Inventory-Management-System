3151006b3f316b4bf595e2c3f36aa08f0e155f7a
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2022-01-02 19:46:07 +0530

    [ProductsApp]: Add serializers for productsapp models

---
+++ productsapp/serializers.py
@@ -0,0 +1,45 @@
+from productsapp.models import Supplier, Category, Product, ProductOrder
+from rest_framework import serializers
+
+
+class SupplierSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = [
+            "name",
+            "address",
+            "phone",
+            "email",
+        ]
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "name",
+            "description",
+        ]
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "price",
+            "supplier_id",
+            "category_id",
+        ]
+
+
+class ProductOrderSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ProductOrder
+        fields = [
+            "customer_id",
+            "product_id",
+            "order_discount",
+            "total_price",
+            "order_date",
+        ]
