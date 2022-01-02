9656ea3e81ed3d8a54703c5322479babe3dd31e0
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2022-01-02 19:31:03 +0530

    [ProductsApp]: Add Product, Category, Supplier and ProductOrder models

--- productsapp/models.py
+++ productsapp/models.py
@@ -1,3 +1,116 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from customersapp.models import Customer
 
-# Create your models here.
+
+class Supplier(models.Model):
+    name = models.CharField(
+        verbose_name="Supplier Name",
+        name="Supplier Name",
+        db_column="supplier_name",
+        max_length=512,
+        blank=False,
+        null=False,
+    )
+    address = models.TextField(
+        blank=True,
+        null=False,
+    )
+    phone = PhoneNumberField(
+        blank=False,
+        null=False,
+    )
+    email = models.EmailField(
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        managed = True
+        db_table = "supplier"
+        verbose_name = "Supplier"
+        verbose_name_plural = "Suppliers"
+
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name="Category Name",
+        name="Category Name",
+        db_column="category_name",
+        max_length=512,
+        blank=False,
+        null=False,
+    )
+    description = models.TextField(
+        blank=True,
+        null=False,
+    )
+
+    class Meta:
+        managed = True
+        db_table = "category"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(
+        verbose_name="Product Name",
+        name="Product Name",
+        db_column="product_name",
+        max_length=512,
+        blank=False,
+        null=False,
+    )
+    price = models.PositiveBigIntegerField(
+        blank=False,
+        null=False,
+    )
+    supplier_id = models.ForeignKey(
+        to=Supplier,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    category_id = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        managed = True
+        db_table = "product"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+class ProductOrder(models.Model):
+    customer_id = models.ForeignKey(
+        to=Customer,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    product_id = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    order_discount = models.IntegerField(blank=True, null=True)
+    total_price = models.PositiveBigIntegerField(
+        blank=False,
+        null=False,
+    )
+    order_date = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=True,
+    )
+
+    class Meta:
+        managed = True
+        db_table = 'product_order'
+        verbose_name = "Product Order"
+        verbose_name_plural = "Product Orders"
